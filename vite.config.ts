126ea2c80b04f1fd1948791cbc21688583a3e2fa
Iovannysm <oenjad@gmail.com>
2025-02-25 20:31:43 -0800

    added files to ready for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mentor-chat/',
   css: {
     modules: {
       localsConvention: 'camelCase'
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true,
+    cssMinify: true
+  },
+  server: {
+    port: 3000
   }
 })
